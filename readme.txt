7a8df1f61dfa29665bfca9e807daaa67939cb46a
Mohanapriya <priya05mca@gmail.com>
2019-05-13 12:03:13 +0100

    practice commit

---
+++ readme.txt
@@ -0,0 +1,2 @@
+open an empty folder in code editor and run npx in the terminal as follows
+	> npx create-react-app .   //create React app in this folder 
---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+//import React from "react";
+import "./App.css";
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <header class="App-first">
+//           <p>Edit src/App.js and save to reload.</p>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
+// const App = function() {
+//   return (
+//     <div>
+//       <header class="App-first">
+//         <p>welcome to react page</p>
+//       </header>
+//     </div>
+//   );
+// };
+
+class App extends Component {
+  render() {
+    const name = this.props.name,
+      age = this.props.age,
+      dat = this.props.dat;
+    return (
+      <div className="App-first">
+        <p>My app Name:{name}</p>
+        <p>My App age:{age} day</p>
+        <p>Date: {dat} </p>
+      </div>
+    );
+  }
+}
+const isTypeString = React.PropTypes.string.isRequired,
+  isTypeNumber = React.PropTypes.number.isRequired;
+App.propTypes = {
+  name: isTypeString,
+  age: isTypeNumber
+};
+App.defaultProps = {
+  age: 2
+};
+export default App;
