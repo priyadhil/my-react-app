feafb3192e48727261e6e54e1c65379adb51fd95
Mohanapriya <priya05mca@gmail.com>
2019-05-13 15:30:32 +0100

    added autobind

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 //import React from "react";
 //import PropTypes from "prop-types";
+import autoBind from "react-autobind";
 import "./App.css";
 
 // class App extends Component {
@@ -25,20 +26,20 @@ import "./App.css";
 //   );
 // };
 
-class App extends Component {
-  render() {
-    const name = this.props.name,
-      age = this.props.age,
-      dat = this.props.dat;
-    return (
-      <div className="App-first">
-        <p>My app Name:{name}</p>
-        <p>My App age:{age} day</p>
-        <p>Date: {dat} </p>
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     const name = this.props.name,
+//       age = this.props.age,
+//       dat = this.props.dat;
+//     return (
+//       <div className="App-first">
+//         <p>My app Name:{name}</p>
+//         <p>My App age:{age} day</p>
+//         <p>Date: {dat} </p>
+//       </div>
+//     );
+//   }
+// }
 //Checking object datatype
 // const isTypeString = React.PropTypes.string.isRequired,
 //   isTypeNumber = React.PropTypes.number.isRequired;
@@ -49,4 +50,61 @@ class App extends Component {
 // App.defaultProps = {
 //   age: 2
 // };
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Priya",
+      subject: "ReactJS",
+      marks: 90,
+      rank: 5
+    };
+    autoBind(
+      this,
+      "updateSubject",
+      "updateMark",
+      "incrementRank",
+      "decrementRank"
+    );
+  }
+  componentDidMount() {
+    console.log("component mounted");
+  }
+  updateSubject(e) {
+    this.setState({
+      subject: e.target.value
+    });
+  }
+  updateMark(e) {
+    this.setState({
+      marks: e.target.value
+    });
+  }
+  incrementRank() {
+    this.setState({
+      rank: this.state.rank + 1
+    });
+  }
+  decrementRank() {
+    this.setState({
+      rank: this.state.rank - 1
+    });
+  }
+  render() {
+    return (
+      <div className="App-first">
+        <h1>State in React - Autobind</h1>
+        <p> Name : {this.state.name}</p>
+        <p> Subject : {this.state.subject}</p>
+        <p> Marks : {this.state.marks}</p>
+        <p> Rank : {this.state.rank}</p>
+        <input type="text" onChange={this.updateSubject} />
+        <input type="number" onChange={this.updateMark} />
+        <button onClick={this.incrementRank}>+</button>
+        <button onClick={this.decrementRank}>-</button>
+      </div>
+    );
+  }
+}
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+//import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <App name="Awesome App" age="1" dat="13/05/19" />,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <App name="Awesome App" age="1" dat="13/05/19" />,
+//   document.getElementById("root")
+// );
+
+render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
